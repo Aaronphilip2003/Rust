ab7fb775311fcb0134c32e2786826cf241c05d56
Aaronphilip2003 <aaronphilip2003@gmail.com>
2026-05-16 17:25:06 +0530

    made a guessing game in rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use std::io::{self, Read};
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io::{self};
 fn main() {
-    println!("Guess the number:");
-    println!("Please input your guess:");
+    println!("This is a guessing game!");
+    println!("Please enter your guess (Numbers are between 1 and 10):");
     let mut guess = String::new();
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    println!("You Guessed {guess}")
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+    println!("The secret number was {secret_number}")
 }
