bd7955ed58385f5225f2a76cdcc618bb7d9cd473
Aaronphilip2003 <aaronphilip2003@gmail.com>
2026-05-21 23:56:15 +0530

    persisting the file

--- file_handling_rust/src/main.rs
+++ file_handling_rust/src/main.rs
@@ -1,5 +1,16 @@
+use std::fs::OpenOptions;
 use std::fs;
+use std::io::Write;
 fn main() {
-    fs::write("database.txt","Hello Database")
-        .expect("Unable to write file");
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("database.txt")
+        .expect("Unable to open file");
+
+    writeln!(file, "Hello Aaron").expect("Unable to write");
+
+    let content = fs::read_to_string("database.txt").expect("Unable to read from file");
+
+    println!("{}", content);
 }
